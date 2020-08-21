ea742f3e028b75998fad398c441583fce8ac89ab
Makoto-stack <55968042+Makoto-stack@users.noreply.github.com>
2020-08-21 11:22:31 -0700

    Update Commands.cpp
    DB almost done but need to figure out how to update inode array entry to null

--- Commands.cpp
+++ Commands.cpp
@@ -111,14 +111,14 @@ void Commands::DF(std::string fileName, directoryFile* directory, Inode* inodeAr
     //Find the Inode
     for (int i = 0; i < 25; i++) {
         if (directory[i].filename == fileName) {
-            directory[i].inodeNum = 0;
-            directory[i].filename = "";
-            for(int j = 0; j <= inodeArray[i].blockCount; j++){
+          for(int j = 0; j <= inodeArray[i].blockCount; j++){
             //inode direct block points to index on disk, which is set back to 0
             *inodeArray[i].directBlocks[j] = 0;
           }
           //bitmap updated, directory entry updated, but still need to update inode array entry to null
-            return;
+          directory[i].inodeNum = 0;
+          directory[i].filename = "";
+          return;
         }
     }
 }
